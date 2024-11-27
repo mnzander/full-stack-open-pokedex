6fe937ebe466c0448a2ebc4d07efa510e3551f04
mnzander <ander.munoz.rivas@zabalburu.org>
2024-11-27 13:55:14 +0100

    e2e tests with playwright

---
+++ tests/pokedex.spec.js
@@ -0,0 +1,24 @@
+const { test, describe, expect } = require("@playwright/test");
+
+describe("Pokedex", () => {
+  test("front page can be opened", async ({ page }) => {
+    await page.goto("http://localhost:8080/pokemon/ivysaur");
+    await expect(page.getByText("ivysaur")).toBeVisible();
+    await expect(
+      page.getByText(
+        "Pokémon and Pokémon character names are trademarks of Nintendo."
+      )
+    ).toBeVisible();
+  });
+  test("pokemon page can be navigated to", async ({ page }) => {
+    await page.goto("http://localhost:8080");
+    await expect(page.getByText("ivysaur")).toBeVisible();
+    await page.goto("http://localhost:8080/pokemon/ivysaur");
+    await expect(page.getByText("ivysaur")).toBeVisible();
+    await expect(
+      page.getByText(
+        "Pokémon and Pokémon character names are trademarks of Nintendo."
+      )
+    ).toBeVisible();
+  });
+});
